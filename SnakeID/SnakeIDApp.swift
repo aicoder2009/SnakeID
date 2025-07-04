01f892d5b8a8723be65f5b9f4d2010f5cb979e67
“Karthick <karthickarun2009@gmail.com>
2025-07-04 09:24:33 -0700

    Initial Commit

---
+++ SnakeID/SnakeIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeIDApp.swift
+//  SnakeID
+//
+//  Created by Karthick Kilash Arun on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SnakeIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnakeIDTests/SnakeIDTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeIDTests.swift
+//  SnakeIDTests
+//
+//  Created by Karthick Kilash Arun on 7/4/25.
+//
+
+import Testing
+@testable import SnakeID
+
+struct SnakeIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
